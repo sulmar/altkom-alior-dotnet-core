e6418d79394136f14215d43480d50e91a2c1ad1c
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-09-30 17:04:02 +0200

    day 1

--- Altkom.DotnetCore.WebApi/Controllers/CustomersController.cs
+++ Altkom.DotnetCore.WebApi/Controllers/CustomersController.cs
@@ -10,15 +10,17 @@ namespace Altkom.DotnetCore.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [Route("api/klienci")]
+    [ApiController]
     public class CustomersController : ControllerBase
     {
         private readonly ICustomerRepository customerRepository;
-        
+
         public CustomersController(ICustomerRepository customerRepository)
         {
             this.customerRepository = customerRepository;
         }
 
+        // api/customers
         [HttpGet]
         public IActionResult Get()
         {
@@ -27,5 +29,31 @@ namespace Altkom.DotnetCore.WebApi.Controllers
             return Ok(customers);
         }
 
+        // api/customers/10
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var customer = customerRepository.Get(id);
+
+            return Ok(customer);
+        }
+
+        // api/customers/Gdansk
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var customer = customerRepository.GetByCity(id);
+
+            return Ok(customer);
+        }
+
+        // api/customers?city=Gdansk&Street=Przymorze
+        [HttpGet]
+        public IActionResult GetByCity([FromQuery] string city, [FromQuery] string street)
+        {
+            var customer = customerRepository.GetByCity(city);
+
+            return Ok(customer);
+        }
     }
 }
